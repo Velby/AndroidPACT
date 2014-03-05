147bb82b4bf733b430b14e264bd3dc8ca02a68f9
Velby <nicolas.dolgin@telecom-paristech.fr>
2014-03-05 17:12:14 +0100

    a whole bunch has changed

--- src/com/example/paquetcoachapp/MainActivity.java
+++ src/com/example/paquetcoachapp/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.paquetcoachapp;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
@@ -7,6 +15,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 	
@@ -31,6 +40,11 @@ public class MainActivity extends Activity {
     	return super.onCreateOptionsMenu(menu);
     }
     
+    public boolean onResume(Menu menu) {
+    	this.refreshData();
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	// Handle presses on the action bar items
@@ -41,11 +55,16 @@ public class MainActivity extends Activity {
     	case R.id.action_settings:
     		openSettings();
     		return true;
+    	case R.id.action_refresh:
+    		refreshData();
+    	case R.id.action_fakePack:
+    		openFakePack();
+    		return true;
     	default:
     		return super.onOptionsItemSelected(item);
     	}
     }
-// modifier les paramètres
+// ouvrir les paramètres
 	private void openSettings() {
 	   	Intent intent = new Intent(this,SettingsActivity.class);
     	startActivity(intent);
@@ -58,8 +77,72 @@ public class MainActivity extends Activity {
     	startActivity(intent);
 		
 	}
+	
+	//lancer le faux paquet
+	private void openFakePack() {
+	   	Intent intent = new Intent(this, FakePack.class);
+    	startActivity(intent);
+		
+	}
 
+	//méthode pour aller voir la consommation en cours
+	public void open_conso(View view) {
+		Intent intent = new Intent(this, ConsoEnCours.class);
+    	startActivity(intent);
+	}
+	
+	//méthode pour ouvrir les stats
+	public void open_stats(View view) {
+		Intent intent = new Intent(this, Stats.class);
+    	startActivity(intent);
+	}
 
+		
+		//méthode pour aller voir le résau social
+	public void open_resau(View view) {
+		Intent intent = new Intent(this, Resau.class);
+    	startActivity(intent);
+	}
+	
+		//Création ou mise a jour du fichier des données
+	public void refreshData()  {
+		try {
+		
+		File appfile = new File(this.getFilesDir(), "donneesAppli");
+		File packfile = new File(this.getFilesDir(), "donneesPack");// pour le FAUX paquet
+		if (!packfile.exists()) {
+			packfile.createNewFile();
+			}
+		
+		if (!appfile.exists()) {
+			appfile.createNewFile();
+		}
+
+		DataInputStream in = null;
+		DataOutputStream out = null;
+		
+			in = new DataInputStream(new FileInputStream(packfile));
+			out = new DataOutputStream(new FileOutputStream(appfile));
+			long date;
+
+			// La méthode read renvoie -1 dès qu'il n'y a plus rien à lire
+			while ((date = in.readLong()) != -1) {
+				out.writeLong(date);
+			}
+			if (in != null)
+				in.close();
+
+			if (out != null)
+				out.close();
+		}
+		 catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
  
     
     

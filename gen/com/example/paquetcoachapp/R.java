656309dd749d2bd60550d103a6952f826b786e85
Velby <nicolas.dolgin@telecom-paristech.fr>
2014-05-02 01:41:17 +0200

    Moins de lag à cause de l'image

--- gen/com/example/paquetcoachapp/R.java
+++ gen/com/example/paquetcoachapp/R.java
@@ -36,10 +36,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int arrows=0x7f020000;
-        public static final int btlogo=0x7f020001;
-        public static final int cigarette=0x7f020002;
-        public static final int ic_smoke=0x7f020003;
-        public static final int main_background=0x7f020004;
+        public static final int background2=0x7f020001;
+        public static final int btlogo=0x7f020002;
+        public static final int cigarette=0x7f020003;
+        public static final int ic_smoke=0x7f020004;
     }
     public static final class id {
         public static final int NumberPicker1=0x7f0a001b;
